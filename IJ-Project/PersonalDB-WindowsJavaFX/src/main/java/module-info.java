735a0d7bfadc5542746b7644583f86a129ec78cc
CaptainPMS <74660767+pigmanms@users.noreply.github.com>
2024-07-19 09:09:24 +0900

    Initial commit
    project initial commit and upload

---
+++ IJ-Project/PersonalDB-WindowsJavaFX/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.myungsuelectronics.personaldbwinjavafx.personaldbwindowsjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.myungsuelectronics.personaldbwinjavafx.personaldbwindowsjavafx to javafx.fxml;
+    exports com.myungsuelectronics.personaldbwinjavafx.personaldbwindowsjavafx;
+}
---
+++ VS-Solution/PersonalDB-WindowsWFA/Form1.cs
@@ -0,0 +1,10 @@
+namespace PersonalDB_WindowsWFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
